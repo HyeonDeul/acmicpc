087e9b2f409864e7c6ecd6fb996b005542517d3a
HyeonDeul <handeul3304@gamil.com>
2022-01-07 16:52:03 +0900

    first commit

--- Gold4/1197.py
+++ Gold4/1197.py
@@ -1,1 +1,11 @@
+from collections import deque
+
 V, E = map(int, input().split())
+
+graph = {i: [] for i in range(1, V+1)}
+
+for _ in range(E):
+    a, b, c = map(int, input().split())
+
+    graph[a].append([b, c])
+    graph[b].append([a, c])
