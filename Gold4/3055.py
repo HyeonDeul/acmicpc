723815522954e4df88bb01523777f7a6d5137688
HyeonDeul <sop369@kakao.com>
2022-02-17 10:14:30 +0900

    fisrt commit

--- Gold4/3055.py
+++ Gold4/3055.py
@@ -1,67 +1,33 @@
 import sys
 from collections import deque
 
-R, C = map(int, sys.stdin.readline().split())
+M, N = map(int, sys.stdin.readline().split())
 graph = []
-disGraph = [[float('inf') for _ in range(C)] for _ in range(R)]
+for _ in range(M):
+    graph.append(list(map(int, sys.stdin.readline().split())))
 
-waters = []
-for i in range(R):
-    temp = list(map(str, sys.stdin.readline().rstrip()))
-
-    for j in range(C):
-        if temp[j] == 'S':
-            S_row, S_col = i, j
-        if temp[j] == 'D':
-            D_row, D_col = i, j
-        if temp[j] == '*':
-            waters.append([i, j])
-    graph.append(temp)
-
-disGraph[S_row][S_col] = 0
-
-cnt = 0
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
-answer = float('inf')
 
-queue = deque([[S_row, S_col, waters, graph]])
+queue = deque([[0, 0]])
+route = 0
 while queue:
-    nowRow, nowCol, nowWaters, temp = queue.pop()
-    nowGraph = [i[:] for i in temp]
-    nowCnt = disGraph[nowRow][nowCol]
+    row, col = queue.popleft()
+    height = graph[row][col]
 
-    new_waters = []
-    while nowWaters:
-        wr, wc = nowWaters.pop()
-        for i in range(4):
-            nextWr = wr+drow[i]
-            nextWc = wc+dcol[i]
-            if not 0 <= nextWr < R:
-                continue
-            if not 0 <= nextWc < C:
-                continue
-            if nowGraph[nextWr][nextWc] not in ['D', '*', 'X']:
-                nowGraph[nextWr][nextWc] = '*'
-                new_waters.append([nextWr, nextWc])
-    nowCnt += 1
+    if row == M-1 and col == N-1:
+        route += 1
+        continue
 
     for i in range(4):
-        nextRow = nowRow+drow[i]
-        nextCol = nowCol+dcol[i]
-        if not 0 <= nextRow < R:
-            continue
-        if not 0 <= nextCol < C:
-            continue
+        nextRow = row+drow[i]
+        nextCol = col+dcol[i]
 
-        next = nowGraph[nextRow][nextCol]
-        if next in ['*', 'X']:
+        if not 0 <= nextRow < M:
             continue
-        elif next == 'D':
-            if nowCnt < answer:
-                answer = nowCnt
-        elif nowCnt < disGraph[nextRow][nextCol]:
-            disGraph[nextRow][nextCol] = nowCnt
-            queue.appendleft([nextRow, nextCol, new_waters[:], nowGraph])
+        if not 0 <= nextCol < N:
+            continue
+        if height > graph[nextRow][nextCol]:
+            queue.append([nextRow, nextCol])
 
-print(answer if answer != float('inf') else 'KAKATUS')
+print(route)
