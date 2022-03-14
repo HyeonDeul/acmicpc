496ab6971ace961e047a74662d8f453c71204ab2
HyeonSoo-Jeon <sop369@kakao.com>
2022-03-14 21:28:48 +0900

    first commit

---
+++ not_solved/12143.py
@@ -0,0 +1,15 @@
+# import sys
+
+# for _ in range(int(sys.stdin.readline())):
+#     eng = []
+#     fran = []
+#     not_define = []
+
+#     n = int(sys.stdin.readline())
+#     eng.extend(list(map(str, sys.stdin.readline().rstrip().split())))
+#     fran.extend(list(map(str, sys.stdin.readline().rstrip().split())))
+
+#     for _ in range(n-2):
+#         temp = list(map(str, sys.stdin.readline().rstrip().split()))
+a = set([1, 2, 3, 4, 5])
+print(a, len(a))
---
+++ not_solved/5052.py
@@ -0,0 +1,15 @@
+import sys
+
+class
+
+for _ in range(int(sys.stdin.readline())):
+    N = int(sys.stdin.readline())
+    ilguanseong = True
+    graph = {}
+
+    for _ in range(N):
+        number = sys.stdin.readline().rstrip()
+        if ilguanseong:
+            dif = False
+            for i in number:
+                if i not in graph
