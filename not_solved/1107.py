58fd0c5420686412f31bce04e07c4bd39bfce1a6
HyeonDeul <sop369@kakao.com>
2022-03-16 18:01:05 +0900

    first commit

--- not_solved/1107.py
+++ not_solved/1107.py
@@ -6,22 +6,26 @@ malfunc = []
 if M != 0:
     malfunc.extend(list(map(int, input().split())))
 
-nearNumU = N
-nearNumD = N
-
-findUpNum = False
-findDownNum = False
 # 다 고장난 경우 - UP, Down
-if len(malfunc) == 10:
+if M == 10:
     print(abs(N-100))
 # 0 빼고 다 고장난 경우 - Up, Down, 0 + Up
-elif len(malfunc) == 9 and 0 not in malfunc:
+elif M == 9 and 0 not in malfunc:
     # 0누르고 방향, 방향
     print(min(N+1, abs(N-100)))
 else:
+
+    nearNumU = N
+    nearNumD = N
+
+    findUpNum = False
+    findDownNum = False
+
     while True:
+
         if findUpNum and findDownNum:
             break
+
         if not findUpNum:
             isMalfunc = False
             for i in str(nearNumU):
@@ -43,7 +47,10 @@ else:
                 findDownNum = True
             else:
                 nearNumD -= 1
+                if nearNumD < 0:
+                    findDownNum = True
+                    nearNumD = float('inf')
 
     UpButton = len(str(nearNumU))+nearNumU-N
-    DownButton = len(str(nearNumD))+N-nearNumD
+    DownButton = len(str(nearNumD))+abs(N-nearNumD)
     print(min(UpButton, DownButton, abs(N-100)))
