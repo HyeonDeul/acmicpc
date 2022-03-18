0bcfad5134deec9196a93c34e79fc375d9990c38
HyeonDeul <sop369@kakao.com>
2022-03-18 13:50:43 +0900

    first commit

--- not_solved/24679.py
+++ not_solved/24679.py
@@ -6,29 +6,39 @@ for _ in range(int(input())):
 
     cnt = 0
 
-    # 0과 1을 맞춰줌
-    gap = (rock[1]-rock[0])//2
-    cnt += gap
-    rock[0] += gap
-    rock[1] -= gap
-    rock[2] -= gap
-    # 1과 2를 맞춰줌
-    gap = 2*((rock[2]-rock[0])//2)
-    cnt += gap
-    rock[2] -= gap
-    # 모두 1,2로 만들어줌
-    toOne = rock[0]-1
-    cnt += 3*toOne
-    rock[0] -= toOne
-    rock[1] -= toOne
-    rock[2] -= toOne
-    rock.sort()
-    # 항목에 따라 나눠줌
-    if rock == [1, 1, 1]:
-        cnt += 1
-    elif rock == [1, 1, 2]:
-        cnt += 3
-    elif rock == [1, 2, 2]:
-        cnt += 4
+    if rock.count(0) == 2:
+        cnt = 0
+    elif rock.count(0) == 1:
+        cnt = rock[1]
+    elif rock.count(1) == 3:
+        cnt = 1
+    elif rock.count(1) > 0:
+        cnt = 0
+    else:
+        if rock[0] % 2 == 1:
+            cnt += 1
+            rock[0] -= 1
+            rock[2] -= 1
+        rock.sort()
+        gap = rock[1]-rock[0]
+        cnt += gap
+        rock[1] -= gap
+        rock[2] -= gap
+
+        # 차이보다 2로가는게 더 빠르다면
+        if 2*(rock[0]-2) <= rock[2]-2:
+            cnt += 1
+        # 2로가는 것보다 차이좁혀지는게 더 빠르다면
+        else:
+            if rock[2] % 2 == 0:
+                # 짝추 차이 좁아지는건 짝수
+                # 즉 3의 배수로 2가 되는게 중요
+                # cnt += ((2*rock[0]-rock[2])-2)//2*3+1
+                if ((2*rock[0]-rock[2])-2) % 4 == 0:
+                    cnt += 1
+            else:
+                if ((2*rock[0]-rock[2]+1)-2) % 4 == 0:
+                    cnt += 1
+                # cnt += ((2*rock[0]-rock[2]+1)-2)//2*3+1
 
     print('B' if cnt % 2 == 1 else 'R')
