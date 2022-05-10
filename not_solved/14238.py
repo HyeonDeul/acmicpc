f548027364867442806396e257000fef8a0c8951
HyeonDeul <sop369@kakao.com>
2022-05-10 16:06:09 +0900

    first commit

--- not_solved/14238.py
+++ not_solved/14238.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 graph = {'A': 0, 'B': 0, 'C': 0}
 
 works = list(map(str, input()))
@@ -5,81 +7,36 @@ days = len(works)
 
 for w in works:
     graph[w] += 1
+cba = 0
+for i in graph:
+    cba = min(cba, graph[i])
 
-answer = ''
-
-canWork = True
-
+answer = 'CBA'*cba
+canMake = False
+que = deque()
+if graph['A'] > 0:
+    que.append(['A', 1, 0, 0])
 if graph['B'] > 0:
-    answer += 'B'
-    graph['B'] -= 1
-elif graph['C'] > 0:
-    answer += 'C'
-    graph['C'] -= 1
-else:
-    answer += 'A'
-    graph['A'] -= 1
+    que.append(['B', 0, 1, 0])
+if graph['C'] > 0:
+    que.append(['C', 0, 0, 1])
 
-if days > 1:
-    if answer[0] == 'B':
-        if graph['C'] > 0:
-            answer += 'C'
-            graph['C'] -= 1
-        elif graph['A'] > 0:
-            answer += 'A'
-            graph['A'] -= 1
-        else:
-            canWork = False
-    elif answer[0] == 'C':
-        if graph['A'] > 0:
-            answer += 'A'
-            graph['A'] -= 1
-        else:
-            canWork = False
-    else:
-        answer += 'A'
-
-
-for day in range(2, days):
-    if not canWork:
+while que:
+    now, a, b, c = que.pop()
+    l = a+b+c
+    if l == days:
+        answer = now
+        canMake = True
         break
+    if graph['A']-a > 0:
+        que.append([now+'A', a+1, b, c])
+    if graph['B']-b > 0 and now[l-1] != 'B':
+        que.append([now+'B', a, b+1, c])
+    if graph['C']-c > 0 and now[l-1] != 'C':
+        if l >= 2:
+            if now[l-2] != 'C':
+                que.append([now+'C', a, b, c+1])
+        else:
+            que.append([now+'C', a, b, c+1])
 
-    if answer[day-2] == 'C':
-        if answer[day-1] == 'B':
-            if graph['A'] > 0:
-                answer += 'A'
-                graph['A'] -= 1
-            else:
-                canWork = False
-        else:
-            if graph['B'] > 0:
-                answer += 'B'
-                graph['B'] -= 1
-            elif graph['A'] > 0:
-                answer += 'A'
-                graph['A'] -= 1
-            else:
-                canWork = False
-    if answer[day-1] == 'B':
-        if graph['C'] > 0:
-            answer += 'C'
-            graph['C'] -= 1
-        elif graph['A'] > 0:
-            answer += 'A'
-            graph['A'] -= 1
-        else:
-            canWork = False
-    else:
-        if graph['B'] > 0:
-            answer += 'B'
-            graph['B'] -= 1
-        elif graph['C'] > 0:
-            answer += 'C'
-            graph['C'] -= 1
-        elif graph['A'] > 0:
-            answer += 'A'
-            graph['A'] -= 1
-        else:
-            canWork = False
-
-print(answer if canWork else -1)
+print(answer if canMake else -1)
