6e54519be304deedea7f95e11f9412c049b788ad
HyeonSoo Jeon <sop369@kakao.com>
2022-05-14 18:15:09 +0900

    first commit

--- not_solved/16235.py
+++ not_solved/16235.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 N, M, K = map(int, input().split())
 
 soils = [[0]*(N+1)]
@@ -6,13 +8,16 @@ for _ in range(N):
     line.extend(list(map(int, input().split())))
     soils.append(line)
 
-trees = [[0 for _ in range(N+1)] for _ in range(N+1)]
+# trees = [[0 for _ in range(N+1)] for _ in range(N+1)]
 tree_list = []
 
 for _ in range(M):
     x, y, z = map(int, input().split())
-    trees[x][y] = z
-    tree_list.append([x, y, z])
+    # trees[x][y] = z
+    tree_list.append([z, x, y])
 
 
 for _ in range(K):
+    tree_list.sort()
+    for z, x, y in tree_list:
+        if soil[x][y] - z < 0:
--- test.cpp
+++ test.cpp
@@ -3,22 +3,47 @@
 
 using namespace std;
 
+const int MAX = 1e5;
+
+class Deque{
+    private :
+        int data[MAX];
+        int idx_front;
+        int idx_back;
+
+    public :
+        Deque(){
+            idx_front = 0;
+            idx_back = 0;
+        }
+        void push_front(int x){
+            data[idx_front] = x;
+            idx_front = (idx_front - 1 + MAX) % MAX;
+        }
+        void push_back(int x){
+            idx_back = (idx_back + 1) % MAX;
+            data[idx_back] = x;
+        }
+        void pop_front(){
+            idx_front = (idx_front +1)% MAX;
+        }
+        void pop_back(){
+            idx_back = (idx_back-1+MAX)% MAX;
+        }
+        int front(){
+            return data[(idx_front + 1) % MAX];
+        }
+        int back(){
+            return data[idx_back];
+        }
+        int size(){
+            return (idx_back - idx_front) % MAX;
+        }
+
+}
+
 int main() {
-    int N, M, p, s;
-    cin >> N >> M;
 
-    int package = 1000, single = 1000;
-
-    for (int i = 0; i < M; i++) {
-        cin >> p >> s;
-        package = min(package, p);
-        single = min(single, s);
-    }
-
-    int price = min((N / 6 + 1) * package,
-                    min(N / 6 * package + N % 6 * single, N * single));
-
-    cout << price;
 
     return 0;
 }
