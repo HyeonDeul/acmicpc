2d8563449c47e8fff89ac536da73bdffcc8cdd53
HyeonSoo Jeon <sop369@kakao.com>
2022-05-21 02:29:02 +0900

    first commit

--- test.py
+++ test.py
@@ -1,64 +1,44 @@
-import heapq
-mycountry = {
-    'A': {'B': 8, 'C': 1, 'D': 2},
-    'B': {},
-    'C': {'B': 5, 'D': 2},
-    'D': {'E': 3, 'F': 5},
-    'E': {'F': 1},
-    'F': {'A': 5}
-}
+V, E = map(int, input().split())
 
+route = []
+for _ in range(E):
+    a, b, c = map(int, input().split())
 
-def dijkstra(road, start):
+    route.append([c, a, b])
 
-    country = {node: float('inf') for node in road}
-    country[start] = 0
-    heap = []
-    heapq.heappush(heap, [country[start], start])
+route.sort()
 
-    while heap:
-        current_distance, now_position = heapq.heappop(heap)
+visit = []
+ans = 0
 
-        if country[now_position] < current_distance:
-            continue
+for dis, no1, no2 in route:
 
-        for arrival, weight in road[now_position].items():
-            distance = current_distance + weight
+    idx1 = -1
+    idx2 = -1
 
-            if distance < country[arrival]:
-                country[arrival] = distance
-                heapq.heappush(heap, [distance, arrival])
+    for i in range(len(visit)):
+        if no1 in visit[i]:
+            idx1 = i
+        if no2 in visit[i]:
+            idx2 = i
 
-    return country
+    if idx1 == -1 and idx2 == -1:
+        visit.append([no1, no2])
+        ans += dis
+    elif idx1 == idx2:
+        continue
+    elif idx1 == -1:
+        visit[idx2].append(no1)
+        ans += dis
+    elif idx2 == -1:
+        visit[idx1].append(no2)
+        ans += dis
+    else:
+        visit[idx1].extend(visit[idx2])
+        visit.pop(idx2)
+        ans += dis
 
+    if len(visit[0]) == V:
+        break
 
-dijkstra(mycountry, 'A')
-
-heap_items = [1, 3, 5, 7, 9]
-
-max_heap = []
-for item in heap_items:
-    heapq.heappush(max_heap, (-item, item))
-
-print(max_heap)
-# 최대힙
-
-N = int(input())
-arr = list(map(int, input().split()))
-
-dp_orm = [1 for _ in range(N)]
-dp_nrm = [1 for _ in range(N)]
-
-for i in range(N):
-    for j in range(i):
-        if arr[i] > arr[j]:
-            dp_orm[i] = max(dp_orm[i], dp_orm[j]+1)
-        if arr[N-i-1] > arr[N-j-1]:
-            dp_nrm[N-i-1] = max(dp_nrm[N-i-1], dp_nrm[N-j-1]+1)
-
-max_arr = 0
-for i in range(N):
-    arrLen = dp_orm[i]+dp_nrm[i]-1
-    if max_arr < arrLen:
-        max_arr = arrLen
-print(max_arr)
+print(ans)
