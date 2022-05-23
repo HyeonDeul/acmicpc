b1ea8c7261c7e81a9541a5f54ca51a425782bf15
HyeonDeul <sop369@kakao.com>
2022-05-23 17:46:16 +0900

    first commit

--- test.py
+++ test.py
@@ -1,44 +1,20 @@
-V, E = map(int, input().split())
+from collections import deque
+import time
 
-route = []
-for _ in range(E):
-    a, b, c = map(int, input().split())
+a = []
+start = time.time()
+for i in range(10000000):
+    a.append(i)
+for _ in range(10000000):
+    a.pop()
+end = time.time()
+print(end-start)
 
-    route.append([c, a, b])
-
-route.sort()
-
-visit = []
-ans = 0
-
-for dis, no1, no2 in route:
-
-    idx1 = -1
-    idx2 = -1
-
-    for i in range(len(visit)):
-        if no1 in visit[i]:
-            idx1 = i
-        if no2 in visit[i]:
-            idx2 = i
-
-    if idx1 == -1 and idx2 == -1:
-        visit.append([no1, no2])
-        ans += dis
-    elif idx1 == idx2:
-        continue
-    elif idx1 == -1:
-        visit[idx2].append(no1)
-        ans += dis
-    elif idx2 == -1:
-        visit[idx1].append(no2)
-        ans += dis
-    else:
-        visit[idx1].extend(visit[idx2])
-        visit.pop(idx2)
-        ans += dis
-
-    if len(visit[0]) == V:
-        break
-
-print(ans)
+b = deque()
+start = time.time()
+for i in range(10000000):
+    b.append(i)
+for _ in range(10000000):
+    b.pop()
+end = time.time()
+print(end-start)
