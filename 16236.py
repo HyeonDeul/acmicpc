34662118590f55fea597cf020484de29754b72ad
Hyeonsoo Jeon <sop369@naver.com>
2024-01-23 17:57:06 +0900

    prob

--- 16236.py
+++ 16236.py
@@ -4,9 +4,12 @@ graph = []
 for _ in range(N):
     graph.append(list(map(int, input().split())))
 
+fish = 0
 for i in range(N):
     for j in range(N):
         if graph[i][j] == 9:
             shark = [i, j]
             graph[i][j] = 0
+        if graph[i][j] != 0:
+            fish += 1
 
